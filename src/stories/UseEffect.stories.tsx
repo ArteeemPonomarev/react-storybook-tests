f86e4d074ec6efc6f0f2c87656d6b17e48e3a63e
ArteeemPonomarev <artponomarev1994@gmail.com>
2021-05-07 00:24:12 +0300

    useEffect types of using

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -1,20 +1,35 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useLayoutEffect, useState} from 'react';
 
 export default {
     title: 'UseEffect demo'
 }
 
 export const SimpleExample = () => {
+    const [fake, setFake] = useState(1)
     const[counter, setCounter] = useState(1);
 
+    console.log("Simple example")
+
     useEffect(() => {
-        console.log('useEffect')
+        console.log('useEffect every render')
+        document.title = counter.toString();
     })
+
+    useEffect(() => {
+        console.log('useEffect only first render (componentDidMount)')
+        document.title = counter.toString();
+    }, [])
+
+    useEffect(() => {
+        console.log('useEffect first render and every counter changing')
+        document.title = counter.toString();
+    }, [counter])
 
     return(
         <>
-            Hello, {counter}
-            <button onClick={() => setCounter(counter + 1)}>+</button>
+            Hello, {counter} {fake}
+            <button onClick={() => setFake(fake + 1)}>fake+</button>
+            <button onClick={() => setCounter(counter + 1)}>counter+</button>
         </>
     )
 }
